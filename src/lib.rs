a73d4c16a1ecad11cc9435d23cff722d9a34caeb
simopek <>
2022-07-11 23:52:34 +0200

    Update code

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.checked_add(Duration::new(1_000_000_000, 0)).unwrap()
+    start + Duration::new(1_000_000_000, 0)
 }
